9a31acd930e924737dcca2b86d70de3066d371e8
SumiAkter2 <akteralisha36@gmail.com>
2023-02-17 13:13:16 +0600

    added cursor animation and comment it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { RouterProvider } from "react-router-dom";
-import "./App.css";
+// import "../public/index.css";
 import router from "./Router/Routes";
 
 function App() {
   return (
     <div className=" mx-auto max-w-screen-xl bg-base-100 " data-theme="dark">
-      <div></div>
       <RouterProvider router={router}> </RouterProvider>
+
+     
+
+     
+     
     </div>
   );
 }
