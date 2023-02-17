c89709443546723cc71e376ef776e4ebf86d3311
SumiAkter2 <akteralisha36@gmail.com>
2023-02-17 13:40:20 +0600

    added star icons

--- src/Pages/Home/Testimonial/Testimonial.js
+++ src/Pages/Home/Testimonial/Testimonial.js
@@ -5,6 +5,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Autoplay, Navigation, Pagination } from "swiper";
+import { AiFillStar } from "react-icons/ai";
+
 const Testimonial = () => {
   const client = [
     {
@@ -63,36 +65,17 @@ const Testimonial = () => {
                     alt="man-img"
                   />
                   <div className="text-white text-start lg:w-1/2 px-6">
-                    <h1 className="text-5xl font-bold">{c.name}</h1>
+                    <h1 className="text-5xl font-bold text-primary">
+                      {c.name}
+                    </h1>
                     <p className="py-6">{c.des}</p>
 
-                    <div className="rating rating-xs">
-                      <input
-                        type="radio"
-                        name="rating-5"
-                        className="mask mask-star-2 bg-orange-400"
-                      />
-                      <input
-                        type="radio"
-                        name="rating-5"
-                        className="mask mask-star-2 bg-orange-400"
-                        checked
-                      />
-                      <input
-                        type="radio"
-                        name="rating-5"
-                        className="mask mask-star-2 bg-orange-400"
-                      />
-                      <input
-                        type="radio"
-                        name="rating-5"
-                        className="mask mask-star-2 bg-orange-400"
-                      />
-                      <input
-                        type="radio"
-                        name="rating-5"
-                        className="mask mask-star-2 bg-orange-400"
-                      />
+                    <div className="flex justify-start items-center">
+                      <AiFillStar fill="orange" />
+                      <AiFillStar fill="orange" />
+                      <AiFillStar fill="orange" />
+                      <AiFillStar fill="orange" />
+                      <AiFillStar fill="orange" />
                     </div>
                     <p>Rating: 5/5</p>
                   </div>
