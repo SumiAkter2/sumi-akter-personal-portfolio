a2778ffdf7610151f48d46767e2f24223f31e7d5
SumiAkter2 <akteralisha36@gmail.com>
2023-02-18 06:51:09 +0600

    add

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import router from "./Router/Routes";
 
 function App() {
   return (
-    <div
-      className="lg:max-w-screen-xl max-w-xs mx-auto  bg-base-100 "
-      data-theme="dark"
-    >
+    <div className="max-w-screen-xl mx-auto  bg-base-100 " data-theme="dark">
       <RouterProvider router={router}> </RouterProvider>
       <ToastContainer />
     </div>
