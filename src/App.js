5458fb0b4209a40c2aaf9151293edd4da88a7cb7
SumiAkter2 <akteralisha36@gmail.com>
2023-02-18 08:40:38 +0600

    done nav add

--- src/App.js
+++ src/App.js
@@ -10,20 +10,36 @@ import LinkButton from "./Pages/Button/LinkButton";
 import router from "./Router/Routes";
 
 function App() {
-  const icon1=<><FaHome/></>
-  const icon2=<><HiInformationCircle/></>
-  const icon3=<><MdWork/></>
-  const icon4=<><MdContactPhone/></>
+  const icon1 = (
+    <>
+      <FaHome />
+    </>
+  );
+  const icon2 = (
+    <>
+      <HiInformationCircle />
+    </>
+  );
+  const icon3 = (
+    <>
+      <MdWork />
+    </>
+  );
+  const icon4 = (
+    <>
+      <MdContactPhone />
+    </>
+  );
   return (
     <div
-      className="max-w-screen-xl mx-auto  bg-base-100 relative"
+      className="max-w-screen-xl mx-auto  bg-base-100 relative "
       data-theme="dark"
     >
       <div
-        className=" bg-transparent fixed top-16 right-3
+        className=" bg-transparent fixed top-[320px] right-3 
       "
       >
-        <LinkButton childrens={icon1}></LinkButton>
+        <LinkButton childrens={icon1}>Home</LinkButton>
         <LinkButton childrens={icon2}>About</LinkButton>
         <LinkButton childrens={icon3}>Contact</LinkButton>
         <LinkButton childrens={icon4}>Portfolio</LinkButton>
